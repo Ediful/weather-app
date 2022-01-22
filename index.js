88c60cbc9fd23454b69415a518656f77028094d1
Ediful <56943939+Ediful@users.noreply.github.com>
2022-01-21 21:51:52 -0600

    feat: switch beteen imperial and metric

--- index.js
+++ index.js
@@ -8,13 +8,32 @@ const weather = {
   windSpeed: null
 };
 
+function handleTemp(tempK) {
+  if (!document.querySelector('input[type=checkbox').checked) {
+    return `${Math.round(tempK - 273.15)}&#176;C`;
+  }
+
+  return `${Math.round((tempK - 273.15) * (9 / 5) + 32)}&#176;F`;
+}
+
+function handleWindSpeed(windSpeed) {
+  if (!document.querySelector('input[type=checkbox').checked) {
+    return `${Math.round(windSpeed * 3.6)} km/h`;
+  }
+
+  return `${Math.round(windSpeed * 2.237)} mi/h`;
+}
+
 function displayWeather() {
   document.getElementById('location').textContent = weather.location;
-  document.getElementById('temp').textContent = weather.temp;
+  document.getElementById('temp').innerHTML = handleTemp(weather.temp);
   document.getElementById('desc').textContent = weather.desc;
-  document.getElementById('main').textContent = weather.main;
-  document.getElementById('humidity').textContent = weather.humidity;
-  document.getElementById('wind-speed').textContent = weather.windSpeed;
+  document.getElementById(
+    'humidity'
+  ).textContent = `${weather.humidity}% humidity`;
+  document.getElementById('wind-speed').textContent = handleWindSpeed(
+    weather.windSpeed
+  );
   document.getElementById(
     'icon-image'
   ).src = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
@@ -23,6 +42,9 @@ function displayWeather() {
 async function getWeatherData() {
   try {
     const search = document.querySelector("input[type='text'");
+
+    // check for comma separated fields
+    console.log(search.value.split(', '));
 
     const response = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${search.value}&APPID=d167055762f8de3624cba9e9eec8fb30`,
@@ -35,7 +57,6 @@ async function getWeatherData() {
     weather.location = search.value;
     weather.desc = openWeatherData.weather[0].description;
     weather.icon = openWeatherData.weather[0].icon;
-    weather.main = openWeatherData.weather[0].main;
     weather.humidity = openWeatherData.main.humidity;
     weather.windSpeed = openWeatherData.wind.speed;
     displayWeather();
@@ -46,6 +67,13 @@ async function getWeatherData() {
 
 getWeatherData();
 
+document.querySelector('input[type=checkbox').addEventListener('click', () => {
+  document.getElementById('temp').innerHTML = handleTemp(weather.temp);
+  document.getElementById('wind-speed').textContent = handleWindSpeed(
+    weather.windSpeed
+  );
+});
+
 document
-  .querySelector("input[type='button']")
+  .querySelector('input[type=button]')
   .addEventListener('click', getWeatherData);
